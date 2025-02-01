2ff9976a9b366d36d9b2ed39fcf9cd8ead597f92
andrew medina <and1alts@gmail.com>
2025-01-31 19:05:42 -0500

    Updated features

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import WES3BudgetTool from "./WES3BudgetTool";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      <WES3BudgetTool />
+    </div>
+  );
+}
